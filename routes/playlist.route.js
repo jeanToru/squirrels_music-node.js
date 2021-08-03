d0db30a12d78d1ecd288ea35110d4f059091cf93
Jean Brown <jeantoruno00@gmail.com>
2021-08-03 08:35:36 -0700

    UPDATE Playlist 2.0

--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -4,7 +4,7 @@ const playlistController = require('../controllers/playlist.controller')
 
 router.post('/playlist', playlistController.create)
 router.get('/playlist/:id', playlistController.getPlaylistUser)
-router.get('/playlist/:id', playlistController.getPlaylist)
+// router.get('/playlist/:id', playlistController.getPlaylist)
 router.put('/playlist', playlistController.upser)
 router.delete('/playlist/:id', playlistController.deletePlaylist);
 router.delete('/playlist/:id/song/:song', playlistController.deleteSong)
