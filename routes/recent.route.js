4ab5bea04dbe73fb297cc55322f16561e52b281e
Jean Brown <jeantoruno00@gmail.com>
2021-08-03 15:00:30 -0700

    Value Recent

--- routes/recent.route.js
+++ routes/recent.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const recentController = require('../controllers/recent.controller')
 
 router.put('/recent', recentController.upser)
-router.get('/recent/:id', recentController.getRecent);
+router.get('/recent/:userId', recentController.getRecent);
 
 module.exports = router;
