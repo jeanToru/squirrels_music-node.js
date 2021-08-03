ab6e0d18c71c90e562ceed37a3786ca3aa9dcbb3
Jean Brown <jeantoruno00@gmail.com>
2021-08-03 09:09:41 -0700

    UPDATE Playlist 2.5.1

--- controllers/playlist.controller.js
+++ controllers/playlist.controller.js
@@ -11,6 +11,16 @@ playlistController.create = async function (req, res, next) {
     }
 }
 
+
+playlistController.getPlaylistUser = async function (req, res, next) {
+    try {
+      const playlist = await playlistService.getPlaylistByUser(req.params);
+      return res.status(200).json({ status: 200, data: playlist, message: 'Successfully playlist retrieved' });
+    } catch (error) {
+      return res.status(400).json({ status: 400, message: error.message });
+    }
+  };
+
 playlistController.getPlaylist = async function(req,res,next){
     try{
         const playlist = await playlistService.getPlaylist(req.params);
--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const playlistController = require('../controllers/playlist.controller')
 
 router.post('/playlist', playlistController.create)
+router.get('/playlist/:idUser', playlistController.getPlaylistUser)
 router.get('/playlist/:id', playlistController.getPlaylist)
 router.put('/playlist', playlistController.upser)
 router.delete('/playlist/:id', playlistController.deletePlaylist);
